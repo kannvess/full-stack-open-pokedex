ee9e9d4772201e64a1b3b56bf54bd24bc798e889
kannvess <kannvess@gmail.com>
2024-03-09 14:15:45 +0700

    11.9: Added e2e testing with Cypress

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-undef */
+describe('Pokedex', () => {
+  it('front page can be opened', function () {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur')
+    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+
+  it('pokemon page can be navigated to', function () {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur')
+
+    cy.get('[href="/pokemon/ivysaur"]').click()
+    cy.contains('chlorophyll')
+  })
+})
